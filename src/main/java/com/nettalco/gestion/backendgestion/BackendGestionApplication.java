9c441b43fa970d3c821d03334c094e46ff36bf4f
Diego Sullcaray Ramos <246810diegosr@gmail.com>
2025-11-23 23:12:51 -0500

    Update BackendGestionApplication to exclude ErrorMvcAutoConfiguration and modify application.properties to disable the default error handling. This allows for custom error handling with CustomErrorController.

--- src/main/java/com/nettalco/gestion/backendgestion/BackendGestionApplication.java
+++ src/main/java/com/nettalco/gestion/backendgestion/BackendGestionApplication.java
@@ -2,8 +2,9 @@ package com.nettalco.gestion.backendgestion;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.web.servlet.error.ErrorMvcAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {ErrorMvcAutoConfiguration.class})
 public class BackendGestionApplication {
 
     public static void main(String[] args) {
