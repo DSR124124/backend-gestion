a12c7b5cc43a238f57a7a5488557967abd9373fe
Diego Sullcaray Ramos <246810diegosr@gmail.com>
2025-11-23 23:14:42 -0500

    dd

--- src/main/java/com/nettalco/gestion/backendgestion/BackendGestionApplication.java
+++ src/main/java/com/nettalco/gestion/backendgestion/BackendGestionApplication.java
@@ -2,9 +2,8 @@ package com.nettalco.gestion.backendgestion;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.web.servlet.error.ErrorMvcAutoConfiguration;
 
-@SpringBootApplication(exclude = {ErrorMvcAutoConfiguration.class})
+@SpringBootApplication
 public class BackendGestionApplication {
 
     public static void main(String[] args) {
